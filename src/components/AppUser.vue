0443505daae385db26facaf3ef47fa1431bae705
- <phuc2333>
2023-04-09 16:21:30 +0700

    'render_data_vuex_for_compionent'

---
+++ src/components/AppUser.vue
@@ -0,0 +1,19 @@
+<template>
+  <User v-for="(user, index) in userList" :key="index" :user='user'></User>
+</template>
+
+<script>
+import User from "./User.vue";
+export default {
+  components: {
+    User,
+  },
+  computed: {
+    userList() {
+      return this.$store.state.user;
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/User.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li>{{user.name}}</li>
+    <li>{{user.age}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props:{
+    user:{
+      type:Object,
+    }
+  }
+};
+</script>
+
+<style></style>
