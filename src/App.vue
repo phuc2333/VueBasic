2002053df0c5acf2a0841ed471d6e99cbfbdae53
- <phuc2333>
2023-04-09 16:59:11 +0700

    'example_computed'

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <button v-on:click="a++">A = A + 1</button>
+    <button v-on:click="b++">B = B + 1</button>
+    <p>Number + A: {{ addA }}</p>
+    <p>Number + B: {{ addB }}</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+export default {
+  data() {
+    return {
+      a: 0,
+      b: 0,
+      number: 20,
+    };
+  },
+
+  computed: {
+    addA() {
+      console.log("add run A:");
+      return this.a + this.number;
+    },
+    addB() {
+      console.log("add run B:");
+      return this.b + this.number;
+    },
+  },
+};
+</script>
+// vvd tren de cho biet dung computed se giup cho toi uu phan render du lieu ra
+dung ham dung su thay doi can thiet // con dung methods se lam cho phan render
+ko dc toi uu // no se chay het du ham day khong dc ban goi
