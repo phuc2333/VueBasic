a3bcd68031a0cfe5a6341e1aebf2f27ef3ca651f
- <phuc2333>
2023-04-13 15:45:41 +0700

    'Cach_viet_mapState'

--- src/components/AppUser.vue
+++ src/components/AppUser.vue
@@ -4,15 +4,36 @@
 
 <script>
 import User from "./User.vue";
+import { mapState } from 'vuex'
 export default {
   components: {
     User,
   },
-  computed: {
-    userList() {
-      return this.$store.state.user;
-    },
-  },
+ // computed: mapState({
+    // c1
+   // userList: (state) => state.user,
+
+    //c2
+    // countAlias: 'user',
+
+  //}),
+
+  // computed:mapState([
+  //   'user'
+  // ]),
+
+  // gio viet như này để phân biệt hàm nào mình xây, hàm nào sẽ có nhiệm vụ lấy dữ
+    // liệu từ trên vueX
+
+    computed: {
+  loading () {
+    return true;
+   },
+  // mix this into the outer object with the object spread operator
+  ...mapState({
+    userList: (state) => state.user,
+  })
+}
 };
 </script>
 
